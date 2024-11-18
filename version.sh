31607fc29480f2016234377fd65a1c5fde4b060f
NodeMafia <kuzmich.ilia00@mail.ru>
2024-11-19 01:04:10 +0300

    File for the current version
     Used for the main script. Version v0.0.386

--- version.sh
+++ version.sh
@@ -1,1 +1,1 @@
-wget https://github.com/block-mesh/block-mesh-monorepo/releases/download/v0.0.387/blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
+wget https://github.com/block-mesh/block-mesh-monorepo/releases/download/v0.0.386/blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
