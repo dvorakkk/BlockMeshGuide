f28dbbb62ab2655db29a926e2f2641289c274f30
ammm <90204443+dvorakkk@users.noreply.github.com>
2024-11-24 21:25:48 +0700

    Update version.sh
    just update node version

--- version.sh
+++ version.sh
@@ -1,1 +1,1 @@
-wget https://github.com/block-mesh/block-mesh-monorepo/releases/download/v0.0.377/blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
+wget https://github.com/block-mesh/block-mesh-monorepo/releases/download/v0.0.400/blockmesh-cli-aarch64-unknown-linux-gnu.tar.gz
